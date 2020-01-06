d8d12781132a76aece65d8b1e64594f330137a50
Yoshio Kanamori <y.k.kanamori@gmail.com>
2020-01-07 01:32:52 +0900

    2

--- gas/server/main.js
+++ gas/server/main.js
@@ -3,7 +3,6 @@ function doGet(request) {
   var tmp = HtmlService.createTemplateFromFile('client/index');
   return tmp.evaluate().setTitle('資産テキストマイニング');
 }
-
 // gasのファイル(html)をインポートする
 // html上で使うときは<?!=include('filename');?>とする
 function include(filename) {
